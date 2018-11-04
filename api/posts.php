00d4b230d318e6e652af51f68b13e42a220d93ab
Rajkumar <rajkumaar2304@gmail.com>
2018-11-04 22:18:37 +0530

    signin,posts API added

---
+++ api/posts.php
@@ -0,0 +1,78 @@
+<?php
+require_once '../intialise.php';
+require_once '../vendor/autoload.php';
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    $jwt=getBearerToken();
+    $user=verify_jwt($jwt);
+    if(isset($user)){
+        $json_res=array();
+        $result=query("select * from posts order by id desc");
+        while ($post = mysqli_fetch_assoc($result)) {
+            $author = "Anonymous";
+            if (isset($post['user_id'])) {
+                $res = query("select name from users where id=" . $post['user_id']);
+                $name = mysqli_fetch_assoc($res);
+                $GLOBALS['author'] = $name['name'];
+            }
+            $id=$post['id'];
+            $title= $post['title'];
+            $content=$post['post'];
+            $date=$post['date'];
+
+            $query = query("select id from users where username='" . $user . "'");
+            $user_id = mysqli_fetch_assoc($query);
+            $current=array(
+                'id'=>$id,
+                'owned'=>($user_id['id']==$post['user_id']) ? true : false,
+                'author'=>$author,
+                'title'=>$title,
+                'content'=>htmlspecialchars_decode($content),
+                'date'=>$date
+            );
+            array_push($json_res,$current);
+        }
+    }else{
+        $json_res=array(
+            'status'=>false,
+            'error'=>"Invalid token"
+        );
+    }
+    header("Content-Type: application/json");
+    echo json_encode($json_res);
+
+}else{
+    header("HTTP/1.1 403 Forbidden");
+    echo "<h1>Error 403 | Access Denied</h1>";
+}
+
+function getAuthorizationHeader(){
+    $headers = null;
+    if (isset($_SERVER['Authorization'])) {
+        $headers = trim($_SERVER["Authorization"]);
+    }
+    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+    } elseif (function_exists('apache_request_headers')) {
+        $requestHeaders = apache_request_headers();
+        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+        //print_r($requestHeaders);
+        if (isset($requestHeaders['Authorization'])) {
+            $headers = trim($requestHeaders['Authorization']);
+        }
+    }
+    return $headers;
+}
+
+function getBearerToken() {
+    $headers = getAuthorizationHeader();
+    // HEADER: Get the access token from the header
+    if (!empty($headers)) {
+        if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+            return $matches[1];
+        }
+    }
+    return null;
+}
+?>
---
+++ api/signin.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once '../intialise.php';
+require_once '../vendor/autoload.php';
+use Firebase\JWT\JWT;
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $username=clean(h($_POST['username']));
+    $password=clean(h($_POST['password']));
+
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 3600;
+    $res=query("select password from users where username='".$username."'");
+    $password_in_db=mysqli_fetch_assoc($res);
+
+    if(!isset($password_in_db)){
+        $error= "Username not found";
+    }
+    else if(strcmp($password_in_db['password'] ,$password)!=0){
+        $error="Invalid password";
+    }
+    if(isset($error)){
+        $result=json_encode(array(
+            'status'=>false,
+            'error'=>$error
+        ));
+    }else{
+        $payload = array(
+            'username' => $username,
+            'iat' => $issuedAt,
+            'exp' => $expirationTime
+        );
+        $key = JWT_KEY;
+        $alg = 'HS256';
+        $jwt = JWT::encode($payload, $key, $alg);
+        $result=json_encode(array(
+            'status'=>true,
+            'token'=>$jwt,
+            'expires_in'=>3600
+        ));
+    }
+    header("Content-Type: application/json");
+    echo $result;
+}else{
+    header("HTTP/1.0 403 Forbidden");
+    echo "<h1>Error 403 | Access Denied</h1>";
+}
